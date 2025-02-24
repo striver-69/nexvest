c50d80e70d09d15a83dc495dd51ff57dfb271fc1
Chiranjivee <chiranjivee960@gmail.com>
2025-02-24 23:58:00 +0530

    updated heading

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          NexVest POC of Deployment
         </p>
         <a
           className="App-link"
