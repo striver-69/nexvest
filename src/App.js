781d3338327dffbc5fbc6e9c92574de8114f3ea3
Chiranjivee <chiranjivee960@gmail.com>
2025-02-25 18:17:42 +0530

    updated changes for cname

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          NexVest POC of Deployment
+          NexVest POC of Deploymentfwefwefewew
         </p>
         <a
           className="App-link"
